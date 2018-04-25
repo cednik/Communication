95171dba0656e79413436961e2c18ad62c9b33f2
Kuba Streit <kuba.streit@gmail.com>
2018-04-26 00:16:32 +0200

    fix exception superclass initialization

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,11 @@
 class ConnectionBrokenError(RuntimeError):
-    def __init__(msg = 'Connection broken.'):
-        RuntimeError.__init__(msg)
+    def __init__(self, msg = 'Connection broken.'):
+        super().__init__(msg)
 
 class PortClosedError(RuntimeError):
-    def __init__(msg = 'Operation on closed port.'):
-        RuntimeError.__init__(msg)
+    def __init__(self, msg = 'Operation on closed port.'):
+        super().__init__(msg)
 
 class DeadlockError(RuntimeError):
-    def __init__(msg):
-        RuntimeError.__init__(msg)
+    def __init__(self, msg):
+        super().__init__(msg)
